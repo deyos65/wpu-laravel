c200690c16e56e86b5242dc444fdef39b12dc244
deyos65 <dedeyosep@mail.unpas.ac.id>
2019-12-04 01:00:17 +0700

    Update & Delete Data

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -45,7 +45,9 @@ class StudentsController extends Controller
 
         $request->validate([
             'nama' =>'required',
-            'nrp' =>'required|size:9'
+            'nrp' =>'required|size:9',
+            'email' =>'required',
+            'jurusan' =>'required'
         ]);
         
         Student::create($request->all());
@@ -71,7 +73,7 @@ class StudentsController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+        return view('students.edit', compact('student'));
     }
 
     /**
@@ -83,7 +85,21 @@ class StudentsController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'nama' =>'required',
+            'nrp' =>'required|size:9',
+            'email' =>'required',
+            'jurusan' =>'required'
+        ]);
+
+        Student::where('id', $student->id)
+            ->update([
+                'nama' =>$request->nama,
+                'nrp' =>$request->nrp,
+                'email' =>$request->email,
+                'jurusan' =>$request->jurusan
+            ]);
+            return redirect('/students')->with('status', 'Data Mahasiswa Berhasil Diubah!');
     }
 
     /**
@@ -94,6 +110,7 @@ class StudentsController extends Controller
      */
     public function destroy(Student $student)
     {
-        //
+        Student::destroy($student->id);
+        return redirect('/students')->with('status', 'Data Mahasiswa Berhasil Dihapus! ');
     }
 }
